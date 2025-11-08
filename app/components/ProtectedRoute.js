a8274de7643b823b0614bf3ddbda9f9d134a7a84
Abdullah <abdullahdeveloper843@gmail.com>
2025-11-08 18:10:33 +0500

    I have made some routes protected.

---
+++ app/components/ProtectedRoute.js
@@ -0,0 +1,25 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import useAuth from "../hooks/useAuth";
+
+const ProtectedRoute = ({children}) => {
+  const session = useAuth();
+  const router = useRouter();
+  
+  useEffect(() => {
+    if (session === null || session === undefined) {
+      router.push("/Auth");
+    }
+  }, [session, router]);
+  
+  // Show loading state while checking auth
+  if (!session) {
+    return <div>Loading...</div>; // or a spinner
+  }
+  
+  return children;
+};
+
+export default ProtectedRoute;
--- app/page.js
+++ app/page.js
@@ -1,11 +1,8 @@
 "use client";
 import TaskManager from "./TaskManager/page";
 import Auth from "./Auth/page";
+import useAuth from "./hooks/useAuth";
 export default function Home() {
-  return (
-    <>
-      <TaskManager />
-      <Auth />
-    </>
-  );
+  const session = useAuth();
+  return <>{session ? <TaskManager /> : <Auth />}</>;
 }
